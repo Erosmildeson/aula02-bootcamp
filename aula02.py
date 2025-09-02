93adcf199f3fb958b8b231071f3410373408888c
Eros <erosmildeson@gmail.com>
2025-09-02 15:10:41 -0400

    fazendo os exercicios da aula02

--- aula02.py
+++ aula02.py
@@ -10,34 +10,12 @@
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo..
 numero_01 = int(input("inserir um nhumero inteiro: "))
 numero_02 = int(input("inserir segundo numero inteiro: "))
-
-
 resultado = numero_01 // numero_02
-
-
 print(resultado)
-
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # #### Números de Ponto Flutuante (`float`)
-
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
 # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
